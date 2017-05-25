d8024d7282402772618e28be455309f15339cf33
eldanesh <kohalmiadam166@gmail.com>
2017-05-25 11:57:10 +0200

    MapGenerator befejezve

--- SpaceAdventure/MapGenerator.cpp
+++ SpaceAdventure/MapGenerator.cpp
@@ -1,11 +1,30 @@
 #include "MapGenerator.h"
 
-std::map<std::string, SolarSystem> MapGenerator::generateMap(std::list<std::string> l)
+//std::string names[] = { "Eltanin", "Avior", "Lesath", "Seginus", "Ogma", "Nap" };
+
+std::map<std::string, SolarSystem> MapGenerator::generateMap(initMap iMap)
 {
 	std::map<std::string, SolarSystem> map = std::map<std::string, SolarSystem>();
-	for (std::list<std::string>::iterator it = l.begin(); it != l.end(); ++it) {
-		SolarSystem s = SolarSystem(*it);
+
+	for (initMap::iterator it = iMap.begin(); it != iMap.end(); it++) {
+		SolarSystem s = SolarSystem(it->first);
+		SolarSystem& ref = s;
+		Randomizer::randomizeSolarSystem(ref);
+		for (std::map<std::string, int>::iterator it2 = it->second.begin(); it2 != it->second.end(); it2++) {
+			s.addNeighbour(it2->first, it2->second);
+		}
+		map.insert(std::pair<std::string, SolarSystem>(it->first, s));
 	}
+
+	/*for (int i = 0; i < numberOfNames; i++) {
+		SolarSystem s = SolarSystem(names[i]);
+		SolarSystem& ref = s;
+		Randomizer::randomizeSolarSystem(ref);
+		if (i < numberOfNames - 1) {
+		//	s.addNeighbour();
+		}
+		map.insert(std::pair<std::string, SolarSystem>(names[i], s));
+	}*/
 	
 	return map;
 }
--- SpaceAdventure/MapGenerator.h
+++ SpaceAdventure/MapGenerator.h
@@ -4,9 +4,13 @@
 #include "Randomizer.h"
 #include <list>
 
+typedef std::map<std::string, std::map<std::string, int>> initMap;
+
 class MapGenerator {
+	//static const int numberOfNames = 6;
+	//std::string names[numberOfNames];
 public:
-	static std::map<std::string, SolarSystem> generateMap(std::list<std::string>);
+	std::map<std::string, SolarSystem> generateMap(std::map<std::string, std::map<std::string, int>>);
 };
 
 
